6cd7fcff8316b3be3d3d6ae6a7e8ceda46709aaa
Ермаков Александр ТКИ-141 <clashsuper010@gmail.com>
2025-12-25 14:42:16 +0300

    Update 1.2.c

--- 1.2.c
+++ 1.2.c
@@ -25,14 +25,30 @@ double trgl(const double a, const double b, const double c);
 */
 int main()
 {
-    double x, y;
-    double a, b, c;
-    printf("Введите стороны прямоугольника: ");
+    double x = 0, y = 0;
+    double a = 0, b = 0, c = 0;
+    printf("Введите стороны прямоугольника (длина и ширина): ");
     scanf("%lf %lf", &x, &y);
+    if (x <= 0 || y <= 0)
+    {
+        printf("Ошибка: стороны прямоугольника должны быть положительными числами\n");
+        return 1;
+    }
     printf("Введите три стороны треугольника: ");
     scanf("%lf %lf %lf", &a, &b, &c);
-    printf("Площадь прямоугольника равна %.2lf\n", prmgl(x, y));
-    printf("Площадь треугольника равна %.2lf\n", trgl(a, b, c));
+    if (a <= 0 || b <= 0 || c <= 0)
+    {
+        printf("Ошибка: стороны треугольника должны быть положительными числами\n");
+        return 1;
+    }
+    if(a + b <= c || a + c <= b || b + c <= a)
+    {
+        printf("Ошибка: такие стороны не могут образовать треугольник\n");
+        return 1;
+    }
+    printf("\nРезультаты вычислений:\n");
+    printf("Площадь прямоугольника (%.2lf x %.2lf) = %.2lf\n", x, y, prmgl(x, y));
+    printf("Площадь треугольника (%.2lf, %.2lf, %.2lf) = %.2lf\n", a, b, c, trgl(a, b, c));
     return 0;
 }
 
@@ -43,6 +59,6 @@ double prmgl(const double x, const double y)
 
 double trgl(const double a, const double b, const double c)
 {
-    double p = (a + b + c) / 2;
+    double p = (a + b + c) / 2; 
     return sqrt(p * (p - a) * (p - b) * (p - c));
 }
