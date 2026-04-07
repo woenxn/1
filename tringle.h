74f96e349503c9d783246b64e5f22d6094c743c7
Ермаков Александр ТКИ-141 <clashsuper010@gmail.com>
2026-04-07 19:43:08 +0300

    Update tringle.h

--- tringle.h
+++ tringle.h
@@ -1,20 +1,86 @@
 #pragma once
 #include "point.h"
 
-class Triangle {
+/**
+ * @brief Класс треугольник
+ */
+class Triangle
+{
 private:
-    Point p1, p2, p3;
-    double sideA, sideB, sideC;
+    /**
+     * @brief Первая вершина
+     */
+    Point p1;
+    /**
+     * @brief Вторая вершина
+     */
+    Point p2;
+    /**
+     * @brief Третья вершина
+     */
+    Point p3;
+
+    /**
+     * @brief Длина стороны BC
+     */
+    double sideA;
+    /**
+     * @brief Длина стороны CA
+     */
+    double sideB;
+    /**
+     * @brief Длина стороны AB
+     */
+    double sideC;
+    /**
+     * @brief Площадь треугольника
+     */
     double area;
+
+    /**
+     * @brief Вычисление сторон и площади
+     */
     void calculate();
+
 public:
+    /**
+     * @brief Конструктор
+     * @param p1 - первая точка
+     * @param p2 - вторая точка
+     * @param p3 - третья точка
+     */
     Triangle(Point p1, Point p2, Point p3);
 
+    /**
+     * @brief Получение длины стороны a(BC)
+     */
     double getSideA() const;
+    /**
+     * @brief Получение длины стороны b(CA)
+     */
     double getSideB() const;
+    /**
+     * @brief Получение длины стороны c(AB)
+     */
     double getSideC() const;
+
+    /**
+     * @brief Нахождение периметра
+     */
     double getPerimeter() const;
+
+    /**
+     * @brief Нахождение площади
+     */
     double getArea() const;
+
+    /**
+     * @brief Расчет радиуса вписанной окружности
+     */
     double getInscribedRadius() const;
+
+    /**
+     * @brief Расчет радиуса описанной окружности
+     */
     double getCircumscribedRadius() const;
 };
