a7d089979b55acaa9029af0575a885d2d3d3eabf
Ермаков Александр ТКИ-141 <clashsuper010@gmail.com>
2026-04-07 19:36:06 +0300

    Update point.h

--- point.h
+++ point.h
@@ -1,12 +1,42 @@
 #pragma once
-
-class Point {
+/**
+ * @brief Класс Точка
+ */
+class Point
+{
 private:
-    double x, y;
+    /**
+     * @brief Координата X
+     */
+    double x;
+    /**
+     * @brief Координата Y
+     */
+    double y;
+
 public:
+    /**
+     * @brief Конструктор точки
+     */
     Point(double x = 0, double y = 0);
+
+    /**
+     * @brief Получение координаты X
+     */
     double getX() const;
+
+    /**
+     * @brief Получение координаты Y
+     */
     double getY() const;
-    bool operator==(const Point& p) const;
-    bool operator!=(const Point& p) const;
+
+    /**
+     * @brief Проверка на равенство оператора
+     */
+    bool operator==(Point p) const;
+
+    /**
+     * @brief Проверка на неравенство оператора
+     */
+    bool operator!=(Point p) const;
 };
