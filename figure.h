efa8b9cb9d6307b81585b21ed297fa8196c49346
Ермаков Александр ТКИ-141 <clashsuper010@gmail.com>
2026-05-02 19:06:43 +0300

    Update figure.h

--- figure.h
+++ figure.h
@@ -3,14 +3,31 @@
 #include <iostream>
 
 /**
- * @brief абстрактный базовый класс "фигура на плоскости"
+ * @brief Абстрактный базовый класс "Фигура на плоскости"
  */
 class Figure
 {
 public:
+    /**
+     * @brief Виртуальный деструктор
+     */
     virtual ~Figure() = default;
 
+    /**
+     * @brief "Рисует" фигуру, выводя её строковое представление в поток
+     * @param os выходной поток (по умолчанию std::cout)
+     */
     virtual void draw(std::ostream& os = std::cout) const = 0;
+
+    /**
+     * @brief Считывает фигуру из входного потока
+     * @param is входной поток (по умолчанию std::cin)
+     */
     virtual void readFromStream(std::istream& is = std::cin) = 0;
+
+    /**
+     * @brief Сериализует фигуру в строку
+     * @return строковое представление фигуры
+     */
     virtual std::string ToString() const = 0;
 };
