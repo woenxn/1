2752bf0ba099247fc0054aeb59c6e03789b87bd9
Ермаков Александр ТКИ-141 <clashsuper010@gmail.com>
2026-05-14 15:29:58 +0300

    Update polygon.cpp

--- polygon.cpp
+++ polygon.cpp
@@ -4,11 +4,23 @@
 #include <cmath>
 #include <sstream>
 
+unsigned int Polygon::maxResolution = 0;
+
+void Polygon::setMaxResolution(unsigned int resolution)
+{
+    if (resolution == 0)
+    {
+        std::cout << "Ошибка. Разрешение экрана должно быть больше 0.\n";
+        exit(1);
+    }
+    maxResolution = resolution;
+}
+
 void Polygon::validate() const
 {
     if (vertices.size() < 3)
     {
-        std::cout << "Ошибка. Многоугольник должен иметь не менее 3 вершин\n";
+        std::cout << "Ошибка. Многоугольник должен иметь не менее 3 вершин.\n";
         exit(1);
     }
     for (size_t i = 0; i < vertices.size(); ++i)
@@ -17,40 +29,51 @@ void Polygon::validate() const
         const Point& p2 = vertices[(i + 1) % vertices.size()];
         if (p1 == p2)
         {
-            std::cout << "Ошибка. Сторона многоугольника имеет нулевую длину\n";
+            std::cout << "Ошибка. Сторона многоугольника имеет нулевую длину.\n";
             exit(1);
         }
     }
 }
 
-Polygon::Polygon(const std::vector<Point>& vertices, unsigned int maxResolution)
-    : vertices(vertices), maxResolution(maxResolution)
+Polygon::Polygon(const std::vector<Point>& vertices)
+    : vertices(vertices)
 {
+    if (maxResolution == 0)
+    {
+        std::cout << "Ошибка. Не установлено разрешение экрана.\n";
+        exit(1);
+    }
     for (const auto& p : vertices)
     {
-        if (p.getX() > maxResolution || p.getY() > maxResolution)
+        if (p.getX() < 0 || p.getY() < 0 || 
+            static_cast<unsigned int>(p.getX()) > maxResolution ||
+            static_cast<unsigned int>(p.getY()) > maxResolution)
         {
-            std::cout << "Ошибка. Вершина выходит за пределы разрешения экрана\n";
+            std::cout << "Ошибка. Вершина выходит за пределы разрешения экрана.\n";
             exit(1);
         }
     }
     validate();
 }
 
-Polygon::Polygon(const unsigned int coords[], size_t count, unsigned int maxResolution)
+Polygon::Polygon(const int coords[], size_t count)
 {
+    if (maxResolution == 0)
+    {
+        std::cout << "Ошибка. Не установлено разрешение экрана (setMaxResolution).\n";
+        exit(1);
+    }
     if (count < 6 || count % 2 != 0)
     {
-        std::cout << "Ошибка. Количество координат должно быть чётным и не менее 6\n";
+        std::cout << "Ошибка. Количество координат должно быть чётным и не менее 6.\n";
         exit(1);
     }
     std::vector<Point> temp;
     for (size_t i = 0; i < count; i += 2)
     {
-        temp.push_back(Point(coords[i], coords[i+1], maxResolution));
+        temp.push_back(Point(coords[i], coords[i+1]));
     }
     vertices = temp;
-    this->maxResolution = maxResolution;
     validate();
 }
 
@@ -73,28 +96,28 @@ void Polygon::draw(std::ostream& os) const
     os << ToString() << std::endl;
 }
 
-Polygon Polygon::readFromStream(std::istream& is, unsigned int maxResolution)
+Polygon Polygon::readFromStream(std::istream& is)
 {
     size_t vertexCount;
     is >> vertexCount;
     if (vertexCount < 3)
     {
-        std::cout << "Ошибка. Количество вершин должно быть не менее 3\n";
+        std::cout << "Ошибка. Количество вершин должно быть не менее 3.\n";
         exit(1);
     }
     std::vector<Point> verts;
     for (size_t i = 0; i < vertexCount; ++i)
     {
-        unsigned int x, y;
+        int x, y;
         is >> x >> y;
-        verts.push_back(Point(x, y, maxResolution));
+        verts.push_back(Point(x, y));
     }
-    return Polygon(verts, maxResolution);
+    return Polygon(verts);
 }
 
 void Polygon::readFromStream(std::istream& is)
 {
-    *this = readFromStream(is, this->maxResolution);
+    *this = readFromStream(is);
 }
 
 std::ostream& operator<<(std::ostream& os, const Polygon& poly)
