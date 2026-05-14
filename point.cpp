377c93a6ff4ee04334e9466d304772de275b24a6
Ермаков Александр ТКИ-141 <clashsuper010@gmail.com>
2026-05-14 15:12:36 +0300

    Update point.cpp

--- point.cpp
+++ point.cpp
@@ -2,21 +2,38 @@
 #include <iostream>
 #include <cstdlib>
 
+unsigned int Point::maxResolution = 0;
+
 Point::Point() : x(0), y(0) {}
 
-Point::Point(unsigned int x, unsigned int y, unsigned int maxResolution)
+Point::Point(int x, int y)
 {
-    if (x > maxResolution || y > maxResolution)
+    if (maxResolution == 0)
     {
-        std::cout << "Ошибка. Координаты точки превышают разрешение экрана\n";
+        std::cout << "Ошибка. Не установлено разрешение экрана.\n";
+        exit(1);
+    }
+    if (x < 0 || y < 0 || static_cast<unsigned int>(x) > maxResolution || static_cast<unsigned int>(y) > maxResolution)
+    {
+        std::cout << "Ошибка. Координаты точки выходят за пределы [0, " << maxResolution << "].\n";
         exit(1);
     }
     this->x = x;
     this->y = y;
 }
 
-unsigned int Point::getX() const { return x; }
-unsigned int Point::getY() const { return y; }
+void Point::setMaxResolution(unsigned int resolution)
+{
+    if (resolution == 0)
+    {
+        std::cout << "Ошибка. Разрешение экрана должно быть больше 0.\n";
+        exit(1);
+    }
+    maxResolution = resolution;
+}
+
+int Point::getX() const { return x; }
+int Point::getY() const { return y; }
 
 bool Point::operator==(const Point& other) const
 {
@@ -26,6 +43,16 @@ bool Point::operator==(const Point& other) const
 bool Point::operator!=(const Point& other) const
 {
     return !(*this == other);
+}
+
+Point Point::operator+(int delta) const
+{
+    return Point(x + delta, y + delta);
+}
+
+Point Point::operator-(int delta) const
+{
+    return Point(x - delta, y - delta);
 }
 
 std::ostream& operator<<(std::ostream& os, const Point& p)
